d5b6c782e507951ee3e3825d43d0e5c63194e074
AshudevGarg <ashudevgarg@gmail.com>
2025-01-29 11:12:53 +0530

    new

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,10 @@
+
+JsUser.greeting = function(){
+  console.log("Hello JS user");
+}
+
+JsUser.greetingTwo = function(){
+  console.log(`Hello JS user,${this.name}`);
+}
+console.log(JsUser.greeting());
+console.log(JsUser.greetingTwo());
